3c80f5fd7f1e65687bd2cef47815b7cc455eaffe
webmarcia <mayito.04@hotmail.com>
2022-10-21 03:54:50 -0300

    se paso el portfolio a angular

---
+++ src/app/app.module.ts
@@ -0,0 +1,33 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './componentes/header/header.component';
+import { SobreMiComponent } from './componentes/sobre-mi/sobre-mi.component';
+import { EducacionExperienciaComponent } from './componentes/educacion-experiencia/educacion-experiencia.component';
+import { HabilidadesComponent } from './componentes/habilidades/habilidades.component';
+import { ProyectosComponent } from './componentes/proyectos/proyectos.component';
+import { ContactoComponent } from './componentes/contacto/contacto.component';
+
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    SobreMiComponent,
+    EducacionExperienciaComponent,
+    HabilidadesComponent,
+    ProyectosComponent,
+    ContactoComponent,
+   
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
